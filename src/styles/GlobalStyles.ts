7aa7ca0f7500cb02308080c86533e6f02d185f2c
w3bh4ck <lucky@softcom.ng>
2021-09-16 23:36:30 +0100

    Core(_): add styled global config

---
+++ src/styles/GlobalStyles.ts
@@ -0,0 +1,42 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyles = createGlobalStyle`
+    @font-face {
+        font-family: "Mulish-Italics";
+        src: url("/fonts/Mulish-Italic-VariableFont_wght.ttf") format("woff");
+        font-style: normal;
+        font-weight: 700;
+    }
+    @font-face {
+        font-family: "Mulish-Regular";
+        src: url("/fonts/Mulish-VariableFont_wght.ttf") format("woff");
+        font-style: normal;
+        font-weight: 600;
+    }
+   
+
+    html {
+        font-size: 10px;
+        --ff-body: "Mulish-Regular";
+        --ff-heading: "Mulish-Regular";
+        scroll-behavior: smooth; 
+    }
+    *, ::before, ::after{
+        margin: 0;
+        padding: 0;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        box-sizing: border-box;
+    }
+    
+
+    body{
+        font-family: var(--ff-body);
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        box-sizing: border-box;
+        line-height: 1.6;
+        margin: 0;
+    }   
+    }
+`;
