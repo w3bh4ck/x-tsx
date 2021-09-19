aa508905a5f6ebfd89d2445d641d2fdafb95584f
w3bh4ck <lucky@softcom.ng>
2021-09-19 21:55:40 +0100

    Update: Add slider dots

--- src/components/cards/SliderTile.tsx
+++ src/components/cards/SliderTile.tsx
@@ -1,49 +1,87 @@
-import { FC } from 'react';
+import React from 'react';
 import styled from 'styled-components';
 import { ClockIcon, LiveIcon } from '../../assets/icons/Icons';
 import StatusButton from '../buttons/StatusButton';
 import { SliderTileProps, StyleProps } from '../../types/types';
 import { formatDistanceToNow, format } from 'date-fns';
 
-const SliderTile: FC<SliderTileProps> = ({
-  topic,
-  status,
-  startAt,
-  tutor,
-  imageUrl,
-}) => {
-  return (
-    <StyledTile bgImage={imageUrl}>
-      <TileInfo>
-        <StatusButton status={status} />
-        <p className="title">{topic}</p>
-        <div className="d-flex">
-          <div className="day-time">
-            <span className="mt-1">
-              <ClockIcon />
-            </span>
-            <span className="mx-2">
-              {formatDistanceToNow(new Date(startAt ? startAt : Date.now()), {
-                addSuffix: true,
-              })}
-            </span>
-            <span>
-              {format(new Date(startAt ? startAt : Date.now()), 'h:m b')}
-            </span>
+const SliderTile = React.forwardRef<HTMLDivElement, SliderTileProps>(
+  (props, ref) => {
+    const { topic, status, startAt, tutor, imageUrl } = props;
+
+    return (
+      <StyledTile ref={ref} bgImage={imageUrl}>
+        <TileInfo>
+          <StatusButton status={status} />
+          <p className="title">{topic}</p>
+          <div className="d-flex">
+            <div className="day-time">
+              <span className="mt-1">
+                <ClockIcon />
+              </span>
+              <span className="mx-2">
+                {formatDistanceToNow(new Date(startAt ? startAt : Date.now()), {
+                  addSuffix: true,
+                })}
+              </span>
+              <span>
+                {format(new Date(startAt ? startAt : Date.now()), 'h:m b')}
+              </span>
+            </div>
+            <div className="day-time mx-2">
+              <span>
+                <LiveIcon />
+              </span>
+              <span className="mx-2 name">
+                {tutor.firstname} {tutor.lastname}
+              </span>
+            </div>
           </div>
-          <div className="day-time mx-2">
-            <span>
-              <LiveIcon />
-            </span>
-            <span className="mx-2 name">
-              {tutor.firstname} {tutor.lastname}
-            </span>
-          </div>
-        </div>
-      </TileInfo>
-    </StyledTile>
-  );
-};
+        </TileInfo>
+      </StyledTile>
+    );
+  }
+);
+
+// const SliderTile: FC<SliderTileProps> = ({
+//   topic,
+//   status,
+//   startAt,
+//   tutor,
+//   imageUrl,
+// }) => {
+//   return (
+//     <StyledTile bgImage={imageUrl}>
+//       <TileInfo>
+//         <StatusButton status={status} />
+//         <p className="title">{topic}</p>
+//         <div className="d-flex">
+//           <div className="day-time">
+//             <span className="mt-1">
+//               <ClockIcon />
+//             </span>
+//             <span className="mx-2">
+//               {formatDistanceToNow(new Date(startAt ? startAt : Date.now()), {
+//                 addSuffix: true,
+//               })}
+//             </span>
+//             <span>
+//               {format(new Date(startAt ? startAt : Date.now()), 'h:m b')}
+//             </span>
+//           </div>
+//           <div className="day-time mx-2">
+//             <span>
+//               <LiveIcon />
+//             </span>
+//             <span className="mx-2 name">
+//               {tutor.firstname} {tutor.lastname}
+//             </span>
+//           </div>
+//         </div>
+//       </TileInfo>
+//     </StyledTile>
+//   );
+// };
 
 export default SliderTile;
 
